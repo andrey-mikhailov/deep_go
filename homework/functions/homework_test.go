a67751d98e38937f9cd552440419275333a249bf
Andrey Mikhailov <mihaylov.andrey31@wildberries.work>
2025-04-26 22:53:39 +0300

    homework 5
    Реализация функций Map, Filter и Reduce для проработки функционального программирования

--- homework/functions/homework_test.go
+++ homework/functions/homework_test.go
@@ -8,18 +8,38 @@ import (
 )
 
 func Map(data []int, action func(int) int) []int {
-	// need to implement
-	return nil
+	if data == nil {
+		return nil
+	}
+	result := make([]int, len(data))
+	for i, v := range data {
+		result[i] = action(v)
+	}
+	return result
 }
 
 func Filter(data []int, action func(int) bool) []int {
-	// need to implement
-	return nil
+	if data == nil {
+		return nil
+	}
+	result := make([]int, 0, len(data))
+	for _, v := range data {
+		if action(v) {
+			result = append(result, v)
+		}
+	}
+	return result
 }
 
 func Reduce(data []int, initial int, action func(int, int) int) int {
-	// need to implement
-	return 0
+	if data == nil {
+		return 0
+	}
+	result := action(0, initial)
+	for _, v := range data {
+		result = action(result, v)
+	}
+	return result
 }
 
 func TestMap(t *testing.T) {
@@ -59,7 +79,8 @@ func TestMap(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			result := Map(test.data, test.action)
-			assert.True(t, reflect.DeepEqual(test.result, result))
+			assert.Equal(t, test.result, result)
+			// assert.True(t, reflect.DeepEqual(test.result, result))
 		})
 	}
 }
